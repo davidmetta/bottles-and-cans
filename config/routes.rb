3c82af38b1042555b0debffd5988e9813425c1cf
david metta <davideliemetta@gmail.com>
2019-08-16 05:59:44 +0300

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
+  get 'home', to: 'pages#home', as: :home
 
   resources :pickups
   get :confirmation, to: 'pickups#confirmation'
